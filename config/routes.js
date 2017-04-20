40205950aae2e32a6e303b1f6d78fa359bd74f83
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-04-20 11:51:39 -0600

    Finished routes.js

---
+++ config/routes.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var passport = require('passport');
+var usersController = require('../controllers/users');
+var staticsController = require('../controllers/statics');
+
+function authenticatedUser(req, res, next) {
+	if (req.isAuthenticated()) return next();
+
+	res.redirect('/');
+}
+
+router.route('/')
+.get(staticsController.home);
+
+router.route('/signup')
+.get(usersController.getSignup)
+.post(usersController.postSignup);
+
+router.route('/login')
+.get(usersController.getLogin)
+.post(usersController.postLogin);
+
+router.router('/logout')
+.get(usersController.getLogout);
+
+module.exports = router
